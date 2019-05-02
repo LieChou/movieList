fbe4cb2977ca9b7de2ddec53c9fbd06366a0efc1
AurelieT <aurelie.tudare@gmail.com>
2019-05-02 12:07:34 +0200

    ajout details films

--- src/components/movie-details/MovieDetails.js
+++ src/components/movie-details/MovieDetails.js
@@ -3,6 +3,18 @@ import React, { Component } from 'react';
 export default class MovieDetails extends Component {
 
     render() {
-        return <div className="w-25 border p-4">Movie Details</div>
+        return (
+            <div className="w-25 border p-4 d-flex flex-column">
+                <h5>{this.props.movie.title}</h5>
+                <div className="d-block mx-auto">
+                    <img src={this.props.movie.img} />
+                </div>
+                <hr className="w-100" />
+                <span className="text-secondary">{this.props.movie.details}</span>
+                <hr className="w-100" />
+                <p>{this.props.movie.description}</p>
+            </div>
+
+        )
     }
 }
--- src/components/movie-list/MovieElement.js
+++ src/components/movie-list/MovieElement.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 
 export default class MovieElement extends Component {
 
+    mouseEnter = () => {
+        this.props.updateSelectedMovie(this.props.movie.title);
+    }
+
     render() {
         return (
-            <div className="w-25 p-2">
-                <div className="card">
-                    <img alt="film Gladiator" src="./img/gladiator.jpeg" className="card-img-top" />
-                    <div className="card-body">
-                        <h5 className="card-title">GLADIATOR</h5>
-                        <p className="card-text">Le général romain Maximus est le plus fidèle soutien de l'empereur Marc Aurèle, qu'il a conduit de victoire en victoire. Jaloux du prestige de Maximus, et plus encore de l'amour que lui voue l'empereur, le fils de Marc Aurèle, Commode, s'arroge brutalement le pouvoir, puis ordonne l'arrestation du général et son exécution. Maximus échappe à ses assassins, mais ne peut empêcher le massacre de sa famille. Capturé par un marchand d'esclaves, il devient gladiateur et prépare sa vengeance.</p>
+            <div onMouseEnter={this.mouseEnter} className="w-50 p-2">
+                <div className="border d-flex">
+                    <img width="150" height="200" alt="film Gladiator" src={this.props.movie.img} />
+                    <div className="flex-fill d-flex flex-column p-3">
+                        <h5>{this.props.movie.title}</h5>
+                        <hr className="w-100" />
+                        <span>{this.props.movie.details}</span>
                     </div>
                 </div>
             </div>
